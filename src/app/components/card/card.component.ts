dfe02c761b7df89f52658d3b2e12156bd8e95b6b
Daniel Echeverri <echeverri121@gmail.com>
2023-12-28 12:30:27 -0500

    Add logic for routing when clicking a card and style basic detail page comp

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Card } from '../../interfaces/card.interface';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
  @Input() card!: Card; 
+ constructor(private router: Router) {}
+ goToCard() {
+  this.router.navigate(['card', this.card.id])
+ }
 }
