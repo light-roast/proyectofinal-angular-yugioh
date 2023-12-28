7abcd5dce7141b3bd2acb1f872b3028f00072830
Daniel Echeverri <echeverri121@gmail.com>
2023-12-28 12:35:18 -0500

    Logic to snapshot the id of the route using ActivatedRoute

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss'
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit{
+  id!: string;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id') || '';
+    console.log(this.id);
+  }
 }
