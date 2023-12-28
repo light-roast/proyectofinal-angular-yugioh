fd3cfb370c618575f2b006b6e92efecaeb6e4e07
Daniel Echeverri <echeverri121@gmail.com>
2023-12-27 19:21:39 -0500

    Primeros components creados, lista la lógica de las routes y las pages que utilizarán

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './pages/list/list.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ListComponent},
+    {path: 'card/:id', component: DetailComponent}
+];
