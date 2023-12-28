e7c5b3bd965886482af28845b655d4f1a46c3315
Daniel Echeverri <echeverri121@gmail.com>
2023-12-27 20:58:39 -0500

    Responsive grid added to list so that cards components can charge

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, NgFor],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
