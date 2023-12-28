bfa9a516846d1c27f14d1eff19cba531f033af10
Daniel Echeverri <echeverri121@gmail.com>
2023-12-28 11:44:00 -0500

    Añadida funcionalidad de búsqueda usando reactiveforms, Formcontrol and debounceTime

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -5,11 +5,13 @@ import { CardService } from '../../services/card.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Card } from '../../interfaces/card.interface';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, NgFor, HttpClientModule, InfiniteScrollModule],
+  imports: [CardComponent, NgFor, HttpClientModule, InfiniteScrollModule, ReactiveFormsModule],
   providers: [CardService],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
@@ -17,6 +19,7 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 export class ListComponent implements OnInit{
   cards: Card[] = [];
   offset = 0;
+  cardTextFC = new FormControl('');
   cardService: CardService = inject(CardService);
 
   onScroll() {
@@ -24,16 +27,24 @@ export class ListComponent implements OnInit{
     this.searchCards();
     }
   
-    searchCards(){
-      this.cardService.getCards(this.offset).subscribe(res => {
+    searchCards(cardName: string | null = null){
+      this.cardService.getCards(cardName, this.offset).subscribe(res => {
         this.cards = [...this.cards, ...res];
   
       })
     }
 
     ngOnInit(): void {
+      // Funcionalidad para la búsqueda, el debounce time retrasa la ejecución del evento value change y se asegura más adelante que el this.cards esté vacío para que solo muestre los resultados
+      this.cardTextFC.valueChanges.pipe(
+        debounceTime(1000)
+      ).subscribe((res) => {
+        this.cards=[];
+        this.searchCards(res)
+      });
+
       this.searchCards();
-    }
+          }
 
   
 }
--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -9,11 +9,12 @@ import { map } from 'rxjs';
 export class CardService {
   API_URL = 'https://db.ygoprodeck.com/api/v7/cardinfo.php';
   constructor(private http: HttpClient) { }
-  getCards(offset = 0) {
-    const params = {
+  getCards(name: string | null, offset = 0) {
+    const params: any = {
       num: 100,
-      offset
+      offset,
     }
+    if(name) params.fname = name;
     // Return the data without the meta key values
     return this.http.get<Card[]>(this.API_URL, {params}).pipe(
       map((res: any)=> {
