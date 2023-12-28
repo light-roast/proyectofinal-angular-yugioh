fc8027c768e59f8ea7177a240e490b203fe505f8
Daniel Echeverri <echeverri121@gmail.com>
2023-12-28 11:08:14 -0500

    Infinitescroll imported in list comp and correctly setup in html and in ts

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -4,11 +4,12 @@ import { NgFor } from '@angular/common';
 import { CardService } from '../../services/card.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Card } from '../../interfaces/card.interface';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, NgFor, HttpClientModule],
+  imports: [CardComponent, NgFor, HttpClientModule, InfiniteScrollModule],
   providers: [CardService],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
@@ -16,6 +17,10 @@ import { Card } from '../../interfaces/card.interface';
 export class ListComponent implements OnInit {
   cards: Card[] = [];
   cardService: CardService = inject(CardService);
+
+  onScroll() {
+    
+  }
 
   ngOnInit(): void {
     this.cardService.getCards().subscribe(res => {
